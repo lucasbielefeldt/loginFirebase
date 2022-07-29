239b2d8ec3a5fe6df90a13397c4e056a13f76a74
Lucas Bielefeldt <lucasbielefeldt@gmail.com>
2022-07-28 21:55:47 -0300

    created firebase config

---
+++ loginFirebase/src/services/firebaseConfig.js
@@ -0,0 +1,13 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCtW88MixdR4ugB9BBtBchhkFmxBIjLR9Q",
+  authDomain: "authfirebase-ea82f.firebaseapp.com",
+  projectId: "authfirebase-ea82f",
+  storageBucket: "authfirebase-ea82f.appspot.com",
+  messagingSenderId: "283380592466",
+  appId: "1:283380592466:web:58c829b36b6eb46b90548b",
+  measurementId: "G-QLHYVV1X48"
+};
+
+const app = initializeApp(firebaseConfig);
